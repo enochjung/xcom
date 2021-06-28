be020472743ff8b3a6605442faee59018e7168df
Enoch Jung <enochjung514@gmail.com>
2021-06-28 15:02:38 +0900

    파일 통합
    완료 버그좀 있는데

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,43 +4,44 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField]
-    private float speed = 20f;
-    [SerializeField]
-    private int health = 3;
-    [SerializeField]
-    private GameObject bullet;
+	[SerializeField]
+	private float speed = 20f;
+	[SerializeField]
+	private int health = 3;
+	[SerializeField]
+	private GameObject bullet;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	// Start is called before the first frame update
+	void Start()
+	{
 
-    // Update is called once per frame
-    void Update()
-    {
-        // 플레이어 이동
-        transform.Translate(Input.GetAxisRaw("Horizontal")*speed*Time.deltaTime, 0, 0);
-        // 발사 (스페이스바)
-        if(Input.GetKeyDown(KeyCode.Space))
-        {
-            GameObject newBullet = Instantiate(bullet, transform.position, Quaternion.identity) as GameObject;
-        }
+	}
 
-    }
+	// Update is called once per frame
+	void Update()
+	{
+		// 플레이어 이동
+		transform.Translate(Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime, 0, 0);
+		// 발사 (스페이스바)
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			GameObject newBullet = Instantiate(bullet, transform.position, Quaternion.identity) as GameObject;
+		}
 
-    // 피격시 호출
-    public void Damaged(int damage)
-    {
-        health = health - damage;
-        //ChangeLife(health);
+	}
 
-        if(health <= 0)
-        {
-            Destroy(this.gameObject);
-            //GameOver();
-        }
-    }
+	// 피격시 호출
+	public void Damaged(int damage)
+	{
+		health = health - damage;
+		Debug.Log("플레이어 피격");
+		//ChangeLife(health);
+
+		if (health <= 0)
+		{
+			Destroy(this.gameObject);
+			//GameOver();
+		}
+	}
 
 }
